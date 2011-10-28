274d9aec0d46492a8b7b549f51e76bc4d8cc564a
Michael Michalak <mmich8266@gmail.com>
2011-10-27 22:21:36 -0500

    Added example json write to the UserServlet.java

--- src/edu/depaul/se491/snotg/service/impl/UserServlet.java
+++ src/edu/depaul/se491/snotg/service/impl/UserServlet.java
@@ -1,12 +1,15 @@
 package edu.depaul.se491.snotg.service.impl;
 
 import java.io.IOException;
+import java.io.StringWriter;
 import java.util.Date;
 
 import javax.servlet.http.*;
 import com.google.appengine.api.users.User;
 import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
+import com.google.appengine.repackaged.org.json.JSONException;
+import com.google.appengine.repackaged.org.json.JSONObject;
 
 import edu.depaul.se491.snotg.UserProfile;
 import edu.depaul.se491.snotg.manager.UserManager;
@@ -30,6 +33,25 @@ public class UserServlet extends HttpServlet {
         }*/
         
         UserProfile userProf = userMgr.findUserProfile(12345L);
-		resp.getWriter().println("Response from GAE User Service - " + new Date());
+        
+        String jsonText = null;
+        try {
+			JSONObject obj=new JSONObject();
+			obj.put("msg","Response from GAE User Service - ");
+			obj.put("ts",new Date().toString());
+			//obj.put("num",new Integer(100));
+			//obj.put("balance",new Double(1000.21));
+			//obj.put("is_vip",new Boolean(true));
+			//obj.put("nickname",null);
+			StringWriter out = new StringWriter();
+			obj.write(out);
+			jsonText = out.toString();
+			System.out.print(jsonText);
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        
+		resp.getWriter().println(jsonText);
     }
 }

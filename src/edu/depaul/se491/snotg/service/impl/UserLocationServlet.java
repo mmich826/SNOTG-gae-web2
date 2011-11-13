c63aa117700e0c85474cca1b1bc3172e7a909c50
Michael Michalak <mmich8266@gmail.com>
2011-11-12 23:37:14 -0600

    GIT merge issues.  Test Commit.  Git sucks

--- src/edu/depaul/se491/snotg/service/impl/UserLocationServlet.java
+++ src/edu/depaul/se491/snotg/service/impl/UserLocationServlet.java
@@ -1,33 +1,29 @@
 package edu.depaul.se491.snotg.service.impl;
 
+import static edu.depaul.se491.snotg.SnotgConstants.INVALID_REQUEST_PARAM_MSG;
+
 import java.io.IOException;
 import java.io.StringWriter;
 import java.util.Date;
 import java.util.List;
 
-import javax.servlet.http.*;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.codehaus.jackson.JsonParseException;
 import org.codehaus.jackson.map.JsonMappingException;
 import org.codehaus.jackson.map.ObjectMapper;
 
-import com.google.appengine.api.users.User;
-import com.google.appengine.api.users.UserService;
-import com.google.appengine.api.users.UserServiceFactory;
 import com.google.appengine.repackaged.org.json.JSONArray;
 import com.google.appengine.repackaged.org.json.JSONException;
 import com.google.appengine.repackaged.org.json.JSONObject;
 
-import edu.depaul.se491.snotg.SnotgConstants;
 import edu.depaul.se491.snotg.UserLocation;
-import edu.depaul.se491.snotg.UserLocationJson;
 import edu.depaul.se491.snotg.UserLocation.Loc;
-import edu.depaul.se491.snotg.UserProfile;
+import edu.depaul.se491.snotg.UserLocationJson;
 import edu.depaul.se491.snotg.manager.UserLocationManager;
-import edu.depaul.se491.snotg.manager.UserManager;
 import edu.depaul.se491.snotg.manager.impl.UserLocationManagerImpl;
-import edu.depaul.se491.snotg.manager.impl.UserManagerImpl;
-import static edu.depaul.se491.snotg.SnotgConstants.*;
 
 /**
  * This class represents a restful service.  The service contracts are:
@@ -102,12 +98,35 @@ public class UserLocationServlet extends HttpServlet {
 	 * This takes and save the current users location and returns
 	 * the list of active user geo locations (as with get_user_locs call).
 	 */
-	private String handleHeartbeat(HttpServletRequest req) throws JsonParseException, JsonMappingException, IOException {
-		UserLocation userLoc = null;
-
-		ObjectMapper mapper = new ObjectMapper();
-//			UserLocationJson user = mapper.readValue(jsonString.getBytes(), UserLocationJson.class);			
-		//mapper.writeValue(System.out, user); // where 'dst' can be File, OutputStream or Writer
+	private String handleHeartbeat(HttpServletRequest req) {
+		UserLocation userLoc = new UserLocation();
+		
+		String reqJson = req.getParameter("request_json");
+		//TODO check for null; Return error
+		
+		try {
+			ObjectMapper mapper = new ObjectMapper();
+			UserLocationJson userJson = mapper.readValue(reqJson.getBytes(), UserLocationJson.class);			
+			//mapper.writeValue(System.out, user); // where 'dst' can be File, OutputStream or Writer
+			// Translate values from Json obj to backend object
+			userLoc.setUserName(userJson.getUserName());
+			UserLocation.Loc loc = new UserLocation.Loc(userJson.getLatit().doubleValue(), userJson.getLongit().doubleValue());
+			userLoc.setLoc(loc);
+			userLoc.setLastUpdated(new Date());
+		} catch (JsonParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (JsonMappingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}     
+		/*userLoc = new UserLocation();
+		userLoc.setUserName("mike");
+		UserLocation.Loc loc = new UserLocation.Loc(123.1234, 555.4444);
+		userLoc.setLoc(loc);*/
 		
 		userLocMgr.updateUserLocation(userLoc);
 		
